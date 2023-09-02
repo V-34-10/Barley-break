516ee88d9445916d9651e4ff35b66be8703ac678
Vlad <vladandryschenko98@gmail.com>
2023-09-02 14:39:07 +0300

    fix UI Hide Dialog

--- app/src/main/java/com/brlea/barley_break/ui/dialog/StartDialogFragment.kt
+++ app/src/main/java/com/brlea/barley_break/ui/dialog/StartDialogFragment.kt
@@ -3,6 +3,8 @@ package com.brlea.barley_break.ui.dialog
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import androidx.fragment.app.DialogFragment
 import com.brlea.barley_break.R
 
@@ -29,12 +31,37 @@ class StartDialogFragment : DialogFragment() {
             }
 
         // Set onCancelListener to dismiss the dialog when clicked outside
+        /*val dialog = builder.create()
+        dialog.setCancelable(false)
+        dialog.setOnShowListener {
+            dialog.window?.decorView?.apply {
+                setOnTouchListener { _, _ -> true }
+                setOnClickListener(null)
+            }
+        }
+
+        return dialog*/
+
+        // Set onCancelListener to dismiss the dialog when clicked outside
         val dialog = builder.create()
         dialog.setCancelable(false)
         dialog.setOnShowListener {
             dialog.window?.decorView?.apply {
                 setOnTouchListener { _, _ -> true }
                 setOnClickListener(null)
+                setOnKeyListener { _, keyCode, _ ->
+                    if (keyCode == KeyEvent.KEYCODE_BACK) {
+                        // Ігнорувати клавішу "Back"
+                        return@setOnKeyListener true
+                    }
+                    return@setOnKeyListener false
+                }
+                dialog.window?.decorView?.systemUiVisibility = (
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        )
             }
         }
 
